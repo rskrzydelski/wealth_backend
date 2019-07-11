587c61edd2d4ebd8c0216314e4f72a08069e2127
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2019-07-11 16:04:10 +0200

    Front end basic stuff
    - css
    - html
    - view

--- wealth/urls.py
+++ wealth/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='home/', permanent=True)),
+    path('home/', views.home_view, name='home'),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('resources/', include(('resources.urls', 'resources'), namespace='resources')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
