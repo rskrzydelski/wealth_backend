d98b3daad14c1e0110914601674b1d495a25e023
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2020-06-30 18:40:16 +0200

    [market] combine tavex service with mongomarket and market-data in the django app

--- tavex-service/app.py
+++ tavex-service/app.py
@@ -1,12 +1,13 @@
 import time
 from tavex import MarketTavexPrices
+import mongomarket
 
 
 gold_spread = 0.96
 silver_spread = 0.85
 
-def tavex_market():
 
+def tavex_market():
     tavex = MarketTavexPrices()
 
     while True:
@@ -15,25 +16,30 @@ def tavex_market():
 
         if gold999oz:
             gold999g = (gold999oz / 31.1) * gold_spread
+            gold999kg = (gold999oz / 31.1) * 1000
             gold585oz = (gold999oz * 0.585) * gold_spread
             gold585g = (gold999g * 0.585) * gold_spread
             gold333oz = (gold999oz * 0.333) * gold_spread
             gold333g = (gold999g * 0.333) * gold_spread
-            print(f"gold999 oz price {gold999oz}")
-            print(f"gold999 g price {gold999g}")
-            print(f"gold585 oz price {gold585oz}")
-            print(f"gold585 g price {gold585g}")
-            print(f"gold333 oz price {gold333oz}")
-            print(f"gold333 g price {gold333g}")
+
+            mongomarket.set_metal_price('gold999oz', gold999oz, 'PLN', 'oz')
+            mongomarket.set_metal_price('gold999g', gold999g, 'PLN', 'g')
+            mongomarket.set_metal_price('gold999kg', gold999kg, 'PLN', 'kg')
+
+            mongomarket.set_metal_price('gold585oz', gold585oz, 'PLN', 'oz')
+            mongomarket.set_metal_price('gold585g', gold585g, 'PLN', 'g')
+            mongomarket.set_metal_price('gold333oz', gold333oz, 'PLN', 'oz')
+            mongomarket.set_metal_price('gold333g', gold333g, 'PLN', 'g')
 
         if silver999oz:
             silver999g = (silver999oz / 31.1) * silver_spread
             silver800oz = (silver999oz * 0.800) * silver_spread
             silver800g = (silver999g * 0.800) * silver_spread
-            print(f"silver999 oz price {silver999oz}")
-            print(f"silver999 g price {silver999g}")
-            print(f"silver800 oz price {silver800oz}")
-            print(f"silver800 g price {silver800g}")
+
+            mongomarket.set_metal_price('silver999oz', silver999oz, 'PLN', 'oz')
+            mongomarket.set_metal_price('silver999g', silver999g, 'PLN', 'g')
+            mongomarket.set_metal_price('silver800oz', silver800oz, 'PLN', 'oz')
+            mongomarket.set_metal_price('silver800g', silver800g, 'PLN', 'g')
 
         time.sleep(60)
 
--- wealth-service/market/market_data.py
+++ wealth-service/market/market_data.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+import mongomarket
 
 
 class MarketData(object):
@@ -7,40 +8,5 @@ class MarketData(object):
 
     @staticmethod
     def get_metal_market_price(name, unit, currency):
-        if name == 'gold999':
-            if unit == 'oz':
-                return Decimal(7300)
-            if unit == 'g':
-                return Decimal(204.5)
-            if unit == 'kg':
-                return Decimal(234726)
-        if name == 'gold585':
-            if unit == 'oz':
-                return Decimal(3732)
-            if unit == 'g':
-                return Decimal(120)
-            if unit == 'kg':
-                return Decimal(120000)
-        if name == 'gold333':
-            if unit == 'oz':
-                return Decimal(2145.9)
-            if unit == 'g':
-                return Decimal(69)
-            if unit == 'kg':
-                return Decimal(69000)
-        if name == 'silver999':
-            if unit == 'oz':
-                return Decimal(100)
-            if unit == 'g':
-                return Decimal(3.2)
-            if unit == 'kg':
-                return Decimal(3200)
-        if name == 'silver800':
-            if unit == 'oz':
-                return Decimal(43.54)
-            if unit == 'g':
-                return Decimal(1.4)
-            if unit == 'kg':
-                return Decimal(1400)
-
-        return Decimal(0)
+        price = mongomarket.get_metal_price(name + unit)
+        return Decimal(price[name + unit]) if price is not None else Decimal(0)
