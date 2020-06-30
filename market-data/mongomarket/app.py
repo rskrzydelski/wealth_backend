8141eed11524765f6caa8866e4216ef6a9ba3563
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2020-06-30 12:59:30 +0200

    [market] Add mongodb as market data storage
    To this storage data will be insert from market services and
    loaded by django.

---
+++ market-data/mongomarket/app.py
@@ -0,0 +1,43 @@
+import pymongo
+
+
+def _get_db():
+    cli = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = cli['market']
+    return db
+
+
+def _list_db():
+    cli = pymongo.MongoClient("mongodb://localhost:27017/")
+    dblist = cli.list_database_names()
+    print(dblist)
+
+
+def _delete_db():
+    cli = pymongo.MongoClient("mongodb://localhost:27017/")
+    cli.drop_database('market')
+
+
+def get_metal_price(name):
+    db = _get_db()
+
+    if not db:
+        return None
+
+    document = db['metals'].find_one({name: {"$exists": True}})
+    return document
+
+
+def set_metal_price(name, value, currency, unit):
+    db = _get_db()
+    query = {name: {"$exists": True}}
+    new_query = {"$set": {name: value, 'currency': currency, 'unit': unit}}
+    db['metals'].update_one(query, new_query, upsert=True)
+
+
+
+
+
+
+
+
