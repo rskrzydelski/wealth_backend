509b3433f5035735973aba33a88dca902bfa6cb2
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2020-07-08 09:47:45 +0200

    [Web] update api endpoints

--- web/src/api/routes.js
+++ web/src/api/routes.js
@@ -1,14 +1,19 @@
 const serverUrl = 'http://127.0.0.1:8000'
 const apiEntry = '/api/v1/'
 
-export const loginUrl = () => `${serverUrl}${apiEntry}auth/login/`
+// authentication endpoints
+export const loginUrl = () => `${serverUrl}${apiEntry}auth/jwt/create/`
+export const TokenRefresh = () => `${serverUrl}${apiEntry}auth/jwt/refresh/`
 export const logoutUrl = () => `${serverUrl}${apiEntry}auth/logout/`
-export const registerUrl = () => `${serverUrl}${apiEntry}auth/registration/`
+export const registerUrl = () => `${serverUrl}${apiEntry}auth/users/`
 
+// market endpoints
 export const marketPriceUrl = () => `${serverUrl}${apiEntry}market`
 
-// dashboard endpoints
+// resources endpoints
 export const getMetalUrl = (name, sum) => `${serverUrl}${apiEntry}resources/metals?name=${name}&sum=${sum}`
 export const getMetalWalletUrl = (name) => `${serverUrl}${apiEntry}wallet/metal/${name}`
 export const getCashlWalletUrl = () => `${serverUrl}${apiEntry}wallet/cash`
 export const getFortuneUrl = () => `${serverUrl}${apiEntry}wallet`
+
+// wallet endpoints
