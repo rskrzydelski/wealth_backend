ec65acb6763d840884810ca44ac837ed13fcede1
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2020-11-06 11:25:59 +0100

    [API] Add crypto endpoints to urls

--- wealth-service/resources/api/urls.py
+++ wealth-service/resources/api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     re_path(r'metals/(?P<pk>\d+)$', views.MetalDetailDelUpdateAPIView.as_view(), name='res-detail'),
     re_path(r'cash$', views.CashLstCreateAPIView.as_view(), name='cash-create-lst'),
     re_path(r'cash/(?P<pk>\d+)$', views.CashDetailUpdateDelAPIView.as_view(), name='cash-detail'),
+    re_path(r'crypto$', views.CryptoLstCreateAPIView.as_view(), name='cr-create-lst'),
+    re_path(r'crypto/(?P<pk>\d+)$', views.CryptoDetailDelUpdateAPIView.as_view(), name='cr-detail'),
 ]
